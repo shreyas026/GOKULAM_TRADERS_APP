9c931da6cd117285005e2206444913c2c27793b0
shreyas026 <shreyassivakumarcse@gmail.com>
2026-07-28 20:21:56 +0530

    fix: remove seed_data call_command, keep migrate only

--- gokulam_backend/wsgi.py
+++ gokulam_backend/wsgi.py
@@ -1,14 +1,14 @@
 import os
-import django
-from django.core.management import call_command
+import sys
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gokulam_backend.settings')
+
+import django
 django.setup()
 
-import sys
+from django.core.management import call_command
 if 'migrate' not in ' '.join(sys.argv):
     call_command('migrate', '--noinput')
-    call_command('seed_data')
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
