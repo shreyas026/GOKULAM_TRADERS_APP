a48069a7749e7ffcde76e877234691c7e7a63f58
shreyas026 <shreyassivakumarcse@gmail.com>
2026-07-28 20:03:26 +0530

    fix: run migrations in wsgi.py on startup

--- gokulam_backend/wsgi.py
+++ gokulam_backend/wsgi.py
@@ -1,16 +1,17 @@
-"""
-WSGI config for gokulam_backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gokulam_backend.settings')
+
+# Run migrations and seed data on startup
+from django.core.management import call_command
+import sys
+
+if 'migrate' not in ' '.join(sys.argv):
+    try:
+        call_command('migrate', '--noinput')
+        call_command('seed_data')
+    except Exception as e:
+        print(f"Startup migration error: {e}")
 
 application = get_wsgi_application()
