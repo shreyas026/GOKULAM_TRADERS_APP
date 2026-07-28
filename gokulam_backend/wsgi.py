c75643affe36ff862f5d1d4aac6d08389e143915
shreyas026 <shreyassivakumarcse@gmail.com>
2026-07-28 20:26:37 +0530

    fix: exec seed_data in wsgi

--- gokulam_backend/wsgi.py
+++ gokulam_backend/wsgi.py
@@ -9,6 +9,8 @@ django.setup()
 from django.core.management import call_command
 if 'migrate' not in ' '.join(sys.argv):
     call_command('migrate', '--noinput')
+    seed_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'seed_data.py')
+    exec(open(seed_path).read())
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
